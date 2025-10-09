232632ef64df6105c739b106659cfd3a2aaa2c14
HikaruYui <diftyaazzahra85@gmail.com>
2025-10-09 08:30:56 +0800

    save progress before pulling

--- post-test-apd-3/2509106042-Diftya_Azzahra-PT3.py
+++ post-test-apd-3/2509106042-Diftya_Azzahra-PT3.py
@@ -15,9 +15,9 @@ else:
         print("Merupakan segitiga sembarang.")
 
     pertanyaan_tambahan = input("Apakah anda ingin menghitung luas segitiga ini? (y/n) ")
-    if pertanyaan_tambahan == "n" or "N":
+    if pertanyaan_tambahan == "n" or pertanyaan_tambahan == "N":
         print("Terima kasih telah menggunakan program ini")
-    if pertanyaan_tambahan == "y" or "Y":
+    if pertanyaan_tambahan == "y" or pertanyaan_tambahan == "Y":
         alas = int(input("Input panjang alas : "))
         tinggi = int(input("Input tinggi : "))
         luas_segitiga = 0.5 * alas * tinggi
